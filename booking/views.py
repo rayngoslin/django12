44d04cf55d0b021998a8826258133c567dd50b10
rayngoslin <ekhlakov.matvey@gmail.com>
2025-10-31 19:08:46 +0100

    Remove backend files from frontend branch

--- booking/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Booking
-
-def booking_list(request):
-    bookings = Booking.objects.all()
-    return render(request, 'booking_list.html', {'bookings': bookings})
